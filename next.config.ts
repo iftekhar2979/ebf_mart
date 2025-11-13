d15444aba9eb1b378987ed65711db8b9bed80729
iftekhar2979 <salminifti79@gmail.com>
2025-11-13 11:16:00 -0600

    chore : esential picture and data added into public directory

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['placehold.co', 'cdn.yoursite.com'], // External image domains you trust
+    domains: ['placehold.co', 'cdn.yoursite.com','images.pexels.com'], // External image domains you trust
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [320, 420, 768, 1024, 1200], // Default responsive breakpoints
     imageSizes: [16, 32, 48, 64, 96],         // Fixed image sizes (icons, avatars, etc.)
